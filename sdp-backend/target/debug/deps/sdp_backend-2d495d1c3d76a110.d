ce98dcc7d0571ab8ff56b6b880b6e691c9ac7179
AYUSHI SHARMA <ayushis380@gmail.com>
2023-01-20 23:45:01 -0700

    Basic rust changes

---
+++ sdp-backend/target/debug/deps/sdp_backend-2d495d1c3d76a110.d
@@ -0,0 +1,5 @@
+/Users/u1369772/Desktop/Projects/secret-diary-backend/sdp-backend/target/debug/deps/sdp_backend-2d495d1c3d76a110: src/main.rs
+
+/Users/u1369772/Desktop/Projects/secret-diary-backend/sdp-backend/target/debug/deps/sdp_backend-2d495d1c3d76a110.d: src/main.rs
+
+src/main.rs:
---
+++ sdp-backend/target/debug/sdp-backend.d
@@ -0,0 +1,1 @@
+/Users/u1369772/Desktop/Projects/secret-diary-backend/sdp-backend/target/debug/sdp-backend: /Users/u1369772/Desktop/Projects/secret-diary-backend/sdp-backend/src/main.rs
